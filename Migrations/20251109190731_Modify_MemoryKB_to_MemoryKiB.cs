a3d170b56d275fe0f1e77b4354c83605281a90a2
zoryon <gioelespata@gmail.com>
2025-11-09 20:08:08 +0100

    Modified KDF Params

---
+++ Migrations/20251109190731_Modify_MemoryKB_to_MemoryKiB.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ZoraVault.Migrations
+{
+    /// <inheritdoc />
+    public partial class Modify_MemoryKB_to_MemoryKiB : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
--- Models/Internal/KdfParams.cs
+++ Models/Internal/KdfParams.cs
@@ -7,16 +7,16 @@ namespace ZoraVault.Models.Internal
         [MaxLength(32)]
         public required string Algorithm { get; set; }
 
-        [Range(100_000, 10_000_000)]
+        [Range(2, 30)]
         public required int Iterations { get; set; }
 
         [Range(128, 4096)]
         public required int KeyLength { get; set; }
 
-        [Range(32, 1024 * 1024)]
-        public required int MemoryKb { get; set; }
+        [Range(32 * 1024, 512 * 1024)]
+        public required int MemoryKiB { get; set; }
 
-        [Range(1, 16)]
+        [Range(1, 64)]
         public required int Parallelism { get; set; }
     }
 }
